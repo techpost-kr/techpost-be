1f628325d2ffa4d35d30113bc939a80190bd499e
동구리 <80397165+dhk5646@users.noreply.github.com>
2025-05-14 22:01:00 +0900

    feature/202505142200 MethodArgumentNotValidException.class Exception 처리 추가 (#1)
    Co-authored-by: dhk5646@gmail.com <>

--- app-api/src/main/java/com/hyeyeoung/study/appapi/common/exception/GlobalExceptionHandler.java
+++ app-api/src/main/java/com/hyeyeoung/study/appapi/common/exception/GlobalExceptionHandler.java
@@ -5,9 +5,14 @@ import com.hyeyeoung.study.common.response.enums.ApiResponseEnum;
 import com.hyeyeoung.study.common.response.exception.ApiResponseException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 @Slf4j
@@ -25,5 +30,20 @@ public class GlobalExceptionHandler {
     public ApiResponse<Void> handleException(Exception ex) {
         log.error("handleException: {}", ex.getMessage(), ex);
         return ApiResponse.fail(ApiResponseEnum.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResponse<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        log.error("handleMethodArgumentNotValidException: {}", ex.getMessage(), ex);
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String defaultMessage = error.getDefaultMessage();
+            errors.put(fieldName, defaultMessage);
+        });
+
+        return ApiResponse.fail(ApiResponseEnum.INTERNAL_SERVER_ERROR, errors);
     }
 }
