198e5a129e5bc60269b3fd3df9fbcb381fe00064
baker.lee <ljw722007@gmail.com>
2025-08-03 17:51:51 +0900

    mysql ddl 추가

--- sql/ddl/mysql.sql
+++ sql/ddl/mysql.sql
@@ -40,3 +40,82 @@ create table Users
     primary key (userSeq)
 ) comment = '사용자';
 
+create table BATCH_JOB_INSTANCE
+(
+    JOB_INSTANCE_ID bigint       not null primary key,
+    VERSION         bigint       null,
+    JOB_NAME        varchar(100) not null,
+    JOB_KEY         varchar(32)  not null,
+    constraint JOB_INST_UN
+        unique (JOB_NAME, JOB_KEY)
+);
+
+create table BATCH_JOB_EXECUTION
+(
+    JOB_EXECUTION_ID bigint        not null auto_increment primary key,
+    VERSION          bigint        null,
+    JOB_INSTANCE_ID  bigint        not null,
+    CREATE_TIME      datetime(6)   not null,
+    START_TIME       datetime(6)   null,
+    END_TIME         datetime(6)   null,
+    STATUS           varchar(10)   null,
+    EXIT_CODE        varchar(2500) null,
+    EXIT_MESSAGE     varchar(2500) null,
+    LAST_UPDATED     datetime(6)   null,
+    constraint JOB_INST_EXEC_FK
+        foreign key (JOB_INSTANCE_ID) references BATCH_JOB_INSTANCE (JOB_INSTANCE_ID)
+);
+
+create table BATCH_JOB_EXECUTION_CONTEXT
+(
+    JOB_EXECUTION_ID   bigint        not null primary key,
+    SHORT_CONTEXT      varchar(2500) not null,
+    SERIALIZED_CONTEXT text          null,
+    constraint JOB_EXEC_CTX_FK
+        foreign key (JOB_EXECUTION_ID) references BATCH_JOB_EXECUTION (JOB_EXECUTION_ID)
+);
+
+create table BATCH_JOB_EXECUTION_PARAMS
+(
+    JOB_EXECUTION_ID bigint        not null,
+    PARAMETER_NAME   varchar(100)  not null,
+    PARAMETER_TYPE   varchar(100)  not null,
+    PARAMETER_VALUE  varchar(2500) null,
+    IDENTIFYING      char          not null,
+    constraint JOB_EXEC_PARAMS_FK
+        foreign key (JOB_EXECUTION_ID) references BATCH_JOB_EXECUTION (JOB_EXECUTION_ID)
+);
+
+create table BATCH_STEP_EXECUTION
+(
+    STEP_EXECUTION_ID  bigint        not null auto_increment primary key,
+    VERSION            bigint        not null,
+    STEP_NAME          varchar(100)  not null,
+    JOB_EXECUTION_ID   bigint        not null,
+    CREATE_TIME        datetime(6)   not null,
+    START_TIME         datetime(6)   null,
+    END_TIME           datetime(6)   null,
+    STATUS             varchar(10)   null,
+    COMMIT_COUNT       bigint        null,
+    READ_COUNT         bigint        null,
+    FILTER_COUNT       bigint        null,
+    WRITE_COUNT        bigint        null,
+    READ_SKIP_COUNT    bigint        null,
+    WRITE_SKIP_COUNT   bigint        null,
+    PROCESS_SKIP_COUNT bigint        null,
+    ROLLBACK_COUNT     bigint        null,
+    EXIT_CODE          varchar(2500) null,
+    EXIT_MESSAGE       varchar(2500) null,
+    LAST_UPDATED       datetime(6)   null,
+    constraint JOB_EXEC_STEP_FK
+        foreign key (JOB_EXECUTION_ID) references BATCH_JOB_EXECUTION (JOB_EXECUTION_ID)
+);
+
+create table BATCH_STEP_EXECUTION_CONTEXT
+(
+    STEP_EXECUTION_ID  bigint        not null primary key,
+    SHORT_CONTEXT      varchar(2500) not null,
+    SERIALIZED_CONTEXT text          null,
+    constraint STEP_EXEC_CTX_FK
+        foreign key (STEP_EXECUTION_ID) references BATCH_STEP_EXECUTION (STEP_EXECUTION_ID)
+);
